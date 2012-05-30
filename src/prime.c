815b2e7664625ed616cb9c1a2b40fba9522b9570
Chris <iambored404@gmail.com>
2012-05-30 13:43:13 -0600

    Working on GETOPT
    Seems to work fairly well so far. I'm not sure how to get -s to take a second optional argument, and I still need to work on long args. Also need a single number-prime check. Otherwise, everything seems to be working fine.
    
    Famous last words.

--- src/prime.c
+++ src/prime.c
@@ -1,6 +1,9 @@
 #include "eratos.h"
 
+#include <errno.h>
 #include <inttypes.h>
+#include <getopt.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,11 +15,67 @@ void usage()
 
 int main(int argc, char** argv)
 {
+	int c;
+	int n_flag = 0;
+	int num = 0;
+	uint64_t num_to_check = 0;
+	FILE* outfile = stdout;
+
 	uint64_t* primes = NULL;
-	int num = (argc == 2) ? strtoll(argv[1], NULL, 0) : 15485863;
-	size_t len = eratos_sieve(num, &primes);
+	size_t len = 0;
 
-	printf("%zu\n", len);
 
+	while((c = getopt(argc, argv, "c:s:o:nh")) != -1)
+	{
+		switch (c)
+		{
+			//check if prime
+			case 'c':
+				sscanf(optarg, "%" SCNu64, &num_to_check);
+				//Now do something with x....
+				fprintf(stderr, "Not implemented.\n");
+				exit(3);
+				break;
+			case 's':
+				num = atoi(optarg);
+				break;
+			case 'o':
+				errno = 0;
+				outfile = fopen(optarg, "w");
+				if(!outfile)
+				{
+					fprintf(stderr, "Error opening %s: %s\n", optarg, strerror(errno));
+					exit(1);
+				}
+				break;
+			case 'n':
+				n_flag = 1;
+				break;
+			case 'h':
+				usage();
+				break;
+			case '?':
+				exit(2);
+				break;
+		}
+	}
+	
+	if(n_flag)
+	{
+		len = eratos_sieve(num, NULL);	
+	}
+	else
+	{
+		len = eratos_sieve(num, &primes);
+	}
+
+	printf("Total: %zu\n", len);
+	
+	for(size_t i = 0; i < len; i++)
+	{
+		fprintf(outfile, "%" PRIu64 "\n", primes[i]);
+	}
+
+	fclose(outfile);
 	return 0;
 }
