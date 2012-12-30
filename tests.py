7672c653e9edf430ef681774c35e15225c798345
Chris <chrisbutler296@gmail.com>
2012-12-29 17:03:13 -0700

    Reorganized Python testing suite.

--- tests.py
+++ tests.py
@@ -2,20 +2,7 @@ import cprimes as cp
 import unittest
 import sys
 
-class TestCprimes(unittest.TestCase):
-	def test_below_0(self):
-		self.assertEqual(cp.below(0), [])
-	
-	def test_below_30(self):
-		self.assertEqual(cp.below(30), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
-	
-	def test_below_15mil(self):
-		self.assertEqual(len(cp.below(15485864)), 1000000)
-		
-	def test_below_TypeErrors(self):
-		with self.assertRaises(TypeError):
-			cp.below(30.0)
-
+class Factor(unittest.TestCase):
 	def test_factor(self):
 		raise NotImplementedError()
 		#Two 8-digit prime factors of Googolplex + 10
@@ -32,21 +19,38 @@ class TestCprimes(unittest.TestCase):
 
 		self.assertEqual(cp.factor(math.factorial(37)), cp.below(38))
 
-	def test_miller_rabin_small_nums(self):
-		limit = 10 ** 6
+class MillerRabin(unittest.TestCase):
+	def test_small_nums(self):
+		limit = 10 ** 4
 		primes = cp.below(limit)
 		rabin_results = [x for x in range(limit) if cp.is_prime(x)]
 		self.assertEqual(primes, rabin_results)
 
-	def test_miller_rabin_big_nums(self):
+	def test_big_prime(self):
 		big_p = 643808006803554439230129854961492699151386107534013432918073439524138264842370630061369715394739134090922937332590384720397133335969549256322620979036686633213903952966175107096769180017646161851573147596390153
 		self.assertTrue(cp.is_prime(big_p))
 
+	def test_big_nonprime(self):
 		not_p = 743808006803554439230129854961492699151386107534013432918073439524138264842370630061369715394739134090922937332590384720397133335969549256322620979036686633213903952966175107096769180017646161851573147596390153
 		self.assertFalse(cp.is_prime(not_p))
 
+	def test_raises_TypeError(self):
 		with self.assertRaises(TypeError):
 			cp.is_prime(557.0)
+
+class Below(unittest.TestCase):
+	def test_0_is_empty(self):
+		self.assertEqual(cp.below(0), [])
+	
+	def test_30_is_correct(self):
+		self.assertEqual(cp.below(30), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+	
+	def test_15mil_is_1mil_long(self):
+		self.assertEqual(len(cp.below(15485864)), 1000000)
+		
+	def test_raises_TypeErrors(self):
+		with self.assertRaises(TypeError):
+			cp.below(30.0)
 
 if __name__ == "__main__":
 	unittest.main(verbosity = 2)
