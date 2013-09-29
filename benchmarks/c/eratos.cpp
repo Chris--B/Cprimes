65dfb62b827b94084c8315767f858ed40144f465
Chris <chrisbutler296@gmail.com>
2013-09-28 19:27:47 -0600

    Benchmarks are closer to being useful... slightly. But they work, and that's the important thing, right now.

--- benchmarks/c/eratos.cpp
+++ benchmarks/c/eratos.cpp
@@ -3,13 +3,21 @@
 
 CELERO_MAIN;
 
-BASELINE(Eratos, Below100, 0, 100) {
+static void run_eratos(size_t num) {
 	uint64_t *primes = nullptr;
 	size_t len = 0;
 	int err;
 
-	celero::DoNotOptimizeAway((err = eratos(1000, &primes, &len)));
+	celero::DoNotOptimizeAway((err = eratos(num, &primes, &len)));
 	if (err) {
-		// error!
+		exit(0);
 	}
+}
+
+BASELINE(Eratos, Below100, 0, 10000) {
+	run_eratos(100);
+}
+
+BENCHMARK(Eratos, Below1000, 0, 10000) {
+	run_eratos(1000);
 }
